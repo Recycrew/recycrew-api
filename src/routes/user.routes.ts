5dbfd0cdf6e34be8c2754734c576f8ec95886540
Larissa1222 <larissa.costa1222@gmail.com>
2022-11-06 13:58:25 -0300

    substituir projeto, excluido antigo

--- src/routes/user.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import UserController from "../controllers/UserController";
-
-export const userRouter = Router();
-
-userRouter.post("/create", UserController.createUser);
