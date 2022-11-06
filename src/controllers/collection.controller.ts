5e95a086a9924dafe48e59f1c794aea27f1efce9
Larissa1222 <larissa.costa1222@gmail.com>
2022-11-06 17:59:30 -0300

    melhorar tratativa erros, melhorar retorno getCollections, com mais detalhes agr

--- src/controllers/collection.controller.ts
+++ src/controllers/collection.controller.ts
@@ -14,19 +14,22 @@ class CollectionController {
       });
 
       if (collectionAlreadyExists) {
-        throw new Error("Collection already exists");
+        return res.status(400).json({ message: "Collection already exists" });
       }
 
       const collect = await prisma.collect.create({
         data: {
           collectorId: collectorId,
-          donationId: donationId
+          donationId: donationId,
         },
       });
-      
-      res.json(collect);
+
+      return res.status(201).json(collect);
+
     } catch (error) {
-      throw new Error("Couldn't create collection");
+      return res
+        .status(500)
+        .json({ error: error, message: "Couldn't create collection" });
     }
   }
 
@@ -34,9 +37,18 @@ class CollectionController {
     try {
       const prisma = new PrismaClient();
 
-      res.json(await prisma.collect.findMany());
+      return res.status(200).json(
+        await prisma.collect.findMany({
+          select: {
+            donation: true,
+            collector: true,
+          },
+        })
+      );
     } catch (error) {
-      throw new Error("Couldn't get collections");
+      return res
+        .status(500)
+        .json({ error: error, message: "Couldn't get collections" });
     }
   }
 }
